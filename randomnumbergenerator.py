30925aeddb0562ea5ed7991b6e233d02a264bf8f
LeVeeHuN <53141474+LeVeeHuN@users.noreply.github.com>
2022-01-18 15:24:57 +0100

    Create randomnumbergenerator.py

---
+++ randomnumbergenerator.py
@@ -0,0 +1,64 @@
+#Made by Levente Bajnóczi
+#2022.01.18 (yyyy.mm.dd)
+
+import random
+
+
+#Set up the default values for the variables
+def setupVars():
+    returnNumbers = False #If you use this script in another script you can define if you want to return the numbers or just print them
+    writeToFile = False #If true the output will be written to a file, otherwise it will be printed to the console
+    showOutput = True #When writeToFile is True, prints the generated numbers to the console
+    minNum = 0 #Determines the minimum number that can be generated
+    maxNum = 100 #Determines the maximum number that can be generated
+    amount = 10 #This amount of random numbers will be generated
+
+
+#Here we take inputs from the user to satisfy their preference
+def userInputs(returnNumbers, writeToFile, showOutput, minNum, maxNum, amount):
+    print("RANDOM SZAM GENERATOR")
+    print("Csak egesz szamokat adj meg minden esetben.")
+    print("Az alapbeallitasok zarojelben lathatoak.")
+    print("Ha nem kivanod modositani akkor hagyd uresen es nyomj entert")
+    minNumtmp = str(input(f"A legkisebb generalhato szam ({minNum}): "))
+    maxNumtmp = str(input(f"A legnagyobb generalhato szam ({maxNum}): "))
+    amounttmp = str(input(f"Ennyi szam legyen generalva ({amount}): "))
+    print("______________________________________________________")
+    print("A kovetkezo kerdesekre csak '0' vagy '1' valaszt adjon")
+    print("0 - Nem, 1 - Igen")
+    writeToFiletmp = str(input(f"Szeretned egy fajlba elmenteni a random generalt elemeket? ({writeToFile}): "))
+    showOutputtmp = str(input(f"Szeretned ha a generalt szamok megjelenitesre kerulnenek? ({showOutput}): "))
+    print("\n\r")
+
+    if minNumtmp == "":
+        del minNumtmp
+    else:
+        try:
+            minNum = int(minNumtmp)
+            del minNumtmp
+        except:
+            userError()
+    if maxNumtmp == "":
+        del maxNumtmp
+    else:
+        try:
+            maxNum = int(maxNumtmp)
+            del maxNumtmp
+
+
+def userError():
+    print("\n\rValami hiba tortent.")
+    print("Nyomj entert a kilepeshez...")
+    input()
+
+
+
+
+#This function will generate the numbers
+def generator(returnNumbers, writeToFile, showOutput, minNum, maxNum, amount):
+    generatedNumbers = []
+    
+
+
+if __name__ == '__main__':
+    setupVars()
